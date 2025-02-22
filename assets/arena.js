c2025c9faa4eb588edf3e3ce9299ac70c06ef19f
Bulgan Altangerel <altab702@newschool.edu>
2025-02-21 21:46:29 -0500

    Reset the arena.js file and fixed the dialog modal
    When I used Claude Ai to troubleshoot initInteraction() function, it caused issues to my audio files. So I deleted it and started again from clean JS template from the demo and fixed it following Eric's loom tutorial.

--- assets/arena.js
+++ assets/arena.js
@@ -1,31 +1,61 @@
-
 // This allows us to process/render the descriptions, which are in Markdown!
 // More about Markdown: https://en.wikipedia.org/wiki/Markdown
 let markdownIt = document.createElement('script')
 markdownIt.src = 'https://cdn.jsdelivr.net/npm/markdown-it@14.0.0/dist/markdown-it.min.js'
 document.head.appendChild(markdownIt)
 
-let channelSlug = 'motion-of-nyc'
 
+
+// Okay, Are.na stuff!
+let channelSlug = 'motion-of-nyc' // The “slug” is just the end of the URL
+
+
+
+// First, let’s lay out some *functions*, starting with our basic metadata:
 let placeChannelInfo = (data) => {
-    let channelTitle = document.getElementById('channel-title')
-    let channelDescription = document.getElementById('channel-description')
-    let channelCount = document.getElementById('channel-count')
-    let channelLink = document.getElementById('channel-link')
+	// Target some elements in your HTML:
+	let channelTitle = document.querySelector('#channel-title')
+	let channelDescription = document.querySelector('#channel-description')
+	let channelCount = document.querySelector('#channel-count')
+	let channelLink = document.querySelector('#channel-link')
 
-    channelTitle.innerHTML = data.title
-    channelDescription.innerHTML = window.markdownit().render(data.metadata.description)
-    channelCount.innerHTML = data.length
-    channelLink.href = `https://www.are.na/channel/${channelSlug}`
+	// Then set their content/attributes to our data:
+	channelTitle.innerHTML = data.title
+	channelDescription.innerHTML = window.markdownit().render(data.metadata.description) // Converts Markdown → HTML
+	channelCount.innerHTML = data.length
+	channelLink.href = `https://www.are.na/channel/${channelSlug}`
 }
 
-let renderBlock = (block) => {
-    let channelBlocks = document.getElementById('channel-blocks')
 
-    // Images - part of "see"
+
+// Then our big function for specific-block-type rendering:
+let renderBlock = (block) => {
+	// To start, a shared `ul` where we’ll insert all our blocks
+	let channelBlocks = document.querySelector('#channel-blocks')
+
+	// Links!
+	if (block.class == 'Link') {
+		let linkItem =
+			`
+			<li class="block-link">
+				
+				<picture>
+					<source media="(max-width: 428px)" srcset="${ block.image.thumb.url }">
+					<source media="(max-width: 640px)" srcset="${ block.image.large.url }">
+					<img src="${ block.image.original.url }">
+				</picture>
+				<h3>${ block.title }</h3>
+				${ block.description_html }
+				<p><a href="${ block.source.url }">See the original ↗</a></p>
+			</li>
+			`
+		channelBlocks.insertAdjacentHTML('beforeend', linkItem)
+	}
+
+	// Images!
     if (block.class == 'Image') {
         let imageItem = 
-        `<li class="block block-image">
+        `<li class="block-image">
             <button>
                 <picture>
                     <source media="(max-width: 428px)" srcset="${block.image.thumb.url}">
@@ -47,28 +77,10 @@ let renderBlock = (block) => {
         channelBlocks.insertAdjacentHTML('beforeend', imageItem)
     }
 
-    // Links and PDFs - part of "read"
-    else if (block.class == 'Link') {
-        let linkItem =
-        `<li class="block block-link">
-        <a href="${block.source.url}">
-            <picture>
-                <source media="(max-width: 428px)" srcset="${block.image.thumb.url}">
-                <source media="(max-width: 640px)" srcset="${block.image.large.url}">
-                <img src="${block.image.original.url}">
-            </picture>
-            <h3>${block.title}</h3>
-             </a>
-           <p> ${block.description_html}</p>
-          
-        </li>`
-        channelBlocks.insertAdjacentHTML('beforeend', linkItem)
-    }
-
-    // Text - part of "read"
+	// Text!
     else if (block.class == 'Text') {
         let textItem = 
-        `<li class="block block-text">
+        `<li class="block-text">
            
             <p>${block.content}</p>
             <p>${block.description_html ? block.description_html : ''}</p>
@@ -77,137 +89,164 @@ let renderBlock = (block) => {
         channelBlocks.insertAdjacentHTML('beforeend', textItem)
     }
 
-    // Attachments (PDF, Audio, Video)
-    else if (block.class == 'Attachment') {
-        let attachment = block.attachment.content_type
+	// Uploaded (not linked) media…
+	else if (block.class == 'Attachment') {
+		let attachment = block.attachment.content_type // Save us some repetition
 
-        // Video - part of "see"
-        if (attachment.includes('video')) {
-            let videoItem =
-            `<li class="block block-video">
-               
-                <video controls src="${block.attachment.url}"></video>
-            </li>`
-            channelBlocks.insertAdjacentHTML('beforeend', videoItem)
-        }
+		// Uploaded videos!
+		if (attachment.includes('video')) {
+			// …still up to you, but we’ll give you the `video` element:
+			let videoItem =
+				`
+				<li>
+					<p><em>Video</em></p>
+					<video controls src="${ block.attachment.url }"></video>
+				</li>
+				`
+			channelBlocks.insertAdjacentHTML('beforeend', videoItem)
+			// More on video, like the `autoplay` attribute:
+			// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/video
+		}
 
-        // PDF - part of "read"
-        else if (attachment.includes('pdf')) {
-            let pdfItem = 
-            `<li class="block block-pdf">
-            <a href="${block.attachment.url}" target="_blank">
-                <picture>
-                    <source media="(max-width: 428px)" srcset="${block.image.thumb.url}">
-                    <source media="(max-width: 640px)" srcset="${block.image.large.url}">
-                    <img src="${block.image.original.url}" alt="${block.title}">
-                </picture>
-                <h3>${block.title}</h3>
-                </a>
-                <p><mark>PDF</mark></p>
-                ${block.description_html ? block.description_html : ''}
-            </li>`
-            channelBlocks.insertAdjacentHTML('beforeend', pdfItem)
-        }
+		// Uploaded PDFs!
+		else if (attachment.includes('pdf')) {
+			// …up to you!
+		}
 
-        // Audio - part of "listen"
-        else if (attachment.includes('audio')) {
-            let audioItem =
-            `<li class="block block-audio">
-            <button>
-            <h3>${block.title}</h3>
-            </button>
-                <dialog>
-                <div class="dialog-style"> 
-                 <h3>${block.title}</h3>
-                 </div>
-                <div class="dialog-content">
-                <audio controls src="${block.attachment.url}">
-                    <source src="${block.attachment.url}" type="audio/mpeg">
-                </audio>
-                    <div class="dialog-text">
+		// Uploaded audio!
+		else if (attachment.includes('audio')) {
+			// …still up to you, but here’s an `audio` element:
+			let audioItem =
+				`
+				  <li class="block-audio">
+				<button class="button">
+                    <h3>${block.title}</h3>
+					<audio controls src="${block.attachment.url}"></audio>
+				</button>
+				<dialog>
+					<h3>${block.title}</h3>
                         <p>${block.description_html ? block.description_html : ''}</p>
-                    </div>
-                    <button class="close">&#x2715;</button>
-                </div>
-            </dialog>
-            </li>`
-            channelBlocks.insertAdjacentHTML('beforeend', audioItem)
-        }
-    }
+					<audio controls src="${block.attachment.url}"></audio>
+					<button class="close">×</button>
+				</dialog>
+				</li>
+				`
+			channelBlocks.insertAdjacentHTML('beforeend', audioItem)
+			// More on audio: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/audio
+		}
+	}
 
-    // Embedded Media (YouTube, etc.)
-    else if (block.class == 'Media') {
-        let embed = block.embed.type
+	// Linked media…
+	else if (block.class == 'Media') {
+		let embed = block.embed.type
 
-        // Video - part of "see"
+		// Linked video!
         if (embed.includes('video')) {
             let linkedVideoItem =
-            `<li class="block block-video">
+            `<li class="block-video">
                
-                ${block.embed.html}
+               ${block.embed.html}
             </li>`
             channelBlocks.insertAdjacentHTML('beforeend', linkedVideoItem)
         }
-    }
+
+		// Linked audio!
+		else if (embed.includes('rich')) {
+			// …up to you!
+		}
+	}
 }
 
-let renderUser = (user, container) => {
-    let userAddress =
-    `<address>
-        <img src="${user.avatar_image.display}">
-        <h3>${user.first_name}</h3>
-        <p><a href="https://are.na/${user.slug}">Are.na profile ↗</a></p>
-    </address>`
-    container.insertAdjacentHTML('beforeend', userAddress)
+
+
+// It‘s always good to credit your work:
+let renderUser = (user, container) => { // You can have multiple arguments for a function!
+	let userAddress =
+		`
+		<address>
+			<img src="${ user.avatar_image.display }">
+			<h3>${ user.first_name }</h3>
+			<p><a href="https://are.na/${ user.slug }">Are.na profile ↗</a></p>
+		</address>
+		`
+	container.insertAdjacentHTML('beforeend', userAddress)
 }
 
 let initInteraction = () => {
-    let blocks = document.querySelectorAll('.block-image, .block-audio, .block-video')
-    blocks.forEach((block) => {
-        let openButton = block.querySelector('button')
-        let dialog = block.querySelector('dialog')
-        let closeButton = dialog.querySelector('.close')
+	let audioBlocks = document.querySelectorAll('.block-audio')
+	audioBlocks.forEach((block) => {
+		let openButton = block.querySelector('button')
+		let dialog = block.querySelector('dialog')
+		let closeButton = dialog.querySelector('button')
+		
+		openButton.onclick = () => {
+			dialog.showModal()
+		}
 
-        openButton.onclick = (e) => {
-            e.preventDefault() // The code was generated using Claude AI software
-            dialog.showModal()
-        }
+		closeButton.onclick = () => {
+			dialog.close()
+		}
 
-        closeButton.onclick = () => {
-            dialog.close()
-        }
+		dialog.onclick = (event) => { 
+			if (event.target == dialog) {
+				dialog.close() 
+			}}
+	})
+    let imageBlocks = document.querySelectorAll('.block-image')
+	imageBlocks.forEach((block) => {
+		let openButton = block.querySelector('button')
+		let dialog = block.querySelector('dialog')
+		let closeButton = dialog.querySelector('button')
+		
+		openButton.onclick = () => {
+			dialog.showModal()
+		}
 
-        // Close dialog when clicking outside
-        // The code from line 167 to 176 were generated using Claude AI software
-        dialog.onclick = (e) => {
-            const dialogDimensions = dialog.getBoundingClientRect()
-            if (
-                e.clientX < dialogDimensions.left ||
-                e.clientX > dialogDimensions.right ||
-                e.clientY < dialogDimensions.top ||
-                e.clientY > dialogDimensions.bottom
-            ) {
-                dialog.close()
-            }
-        }
-    })
+		closeButton.onclick = () => {
+			dialog.close()
+		}
+
+		dialog.onclick = (event) => { 
+			if (event.target == dialog) {
+				dialog.close() 
+			}}
+	})
+    let videoBlocks = document.querySelectorAll('.block-video')
+	videoBlocks.forEach((block) => {
+		let openButton = block.querySelector('button')
+		let dialog = block.querySelector('dialog')
+		let closeButton = dialog.querySelector('button')
+		
+		openButton.onclick = () => {
+			dialog.showModal()
+		}
+
+		closeButton.onclick = () => {
+			dialog.close()
+		}
+
+		dialog.onclick = (event) => { 
+			if (event.target == dialog) {
+				dialog.close() 
+			}}
+	})
 }
 
-
-
+// Now that we have said what we can do, go get the data:
 fetch(`https://api.are.na/v2/channels/${channelSlug}?per=100`, { cache: 'no-store' })
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data)
-        placeChannelInfo(data)
+	.then((response) => response.json()) // Return it as JSON data
+	.then((data) => { // Do stuff with the data
+		console.log(data) // Always good to check your response!
+		placeChannelInfo(data) // Pass the data to the first function
 
-        data.contents.reverse().forEach((block) => {
-            renderBlock(block)
-        })
-
+		// Loop through the `contents` array (list), backwards. Are.na returns them in reverse!
+		data.contents.reverse().forEach((block) => {
+			// console.log(block) // The data for a single block
+			renderBlock(block) // Pass the single block data to the render function
+		})
         initInteraction()
-
-        let channelUsers = document.getElementById('channel-users')
-        data.collaborators.forEach((collaborator) => renderUser(collaborator, channelUsers))
-        renderUser(data.user, channelUsers)
-    })
+		// Also display the owner and collaborators:
+		let channelUsers = document.querySelector('#channel-users') // Show them together
+		data.collaborators.forEach((collaborator) => renderUser(collaborator, channelUsers))
+		renderUser(data.user, channelUsers)
+	})
